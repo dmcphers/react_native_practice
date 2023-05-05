ac7d96ce9f957c81472f27507fc3c9bc3b3d4b71
Drew McPherson <drewmac1971@gmail.com>
2023-05-04 22:53:29 -0500

    using redux

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -8,7 +8,15 @@ import HomeScreen from "./HomeScreen";
 import AboutScreen from "./AboutScreen";
 import ContactScreen from "./ContactScreen";
 import { Icon } from "react-native-elements";
-import logo from "../assets/images/logo.png"
+import logo from "../assets/images/logo.png";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchPartners } from '../features/partners/partnersSlice';
+import { fetchCampsites } from '../features/campsites/campsitesSlice';
+import { fetchPromotions } from '../features/promotions/promotionsSlice';
+import { fetchComments } from '../features/comments/commentsSlice';
+
+
 
 const Drawer = createDrawerNavigator();
 
@@ -134,6 +142,16 @@ const CustomDrawerContent = (props) => (
 )
 
 const Main = () => {
+    const dispatch = useDispatch();
+    dispatch(fetchCampsites());
+    dispatch(fetchPromotions());
+    dispatch(fetchPartners());
+    dispatch(fetchComments());
+
+
+    useEffect(() => {
+
+    }, [dispatch]);
 
     return (
         <View style={{
