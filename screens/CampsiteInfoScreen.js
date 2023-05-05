3e2bd63d417de0317a901b5099a376e774249ffc
Drew McPherson <drewmac1971@gmail.com>
2023-05-05 14:02:29 -0500

    redux adding favorites

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,15 +1,17 @@
-import { useState } from 'react';
 import { FlatList, Text, StyleSheet, View } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
+import { toggleFavorite } from '../features/favorites/favoritesSlice';
 
 
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
     const comments = useSelector((state => state.comments));
+    const favorites = useSelector((state => state.favorites));
+    const dispatch = useDispatch();
 
-    const [favorite, setFavorite] = useState(false);
+
 
     const renderCommentItem = ({ item }) => {
         return (
@@ -38,8 +40,9 @@ const CampsiteInfoScreen = ({ route }) => {
                 <>
                     <RenderCampsite
                         campsite={campsite}
-                        isFavorite={favorite}
-                        markFavorite={() => setFavorite(true)}
+                        isFavorite={favorites.includes(campsite.id)}
+                        markFavorite={() => dispatch(toggleFavorite(campsite.id))
+                        }
                     />
                     <Text style={styles.commentsTitle}>Comments</Text>
                 </>
