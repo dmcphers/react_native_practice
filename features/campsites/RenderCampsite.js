8f4471aa4e65d5ec1712b90e754178b1825d040c
Drew McPherson <drewmac1971@gmail.com>
2023-05-11 14:58:54 -0500

    Gestures part 2

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -2,20 +2,32 @@ import { StyleSheet, Text, View, PanResponder, Alert } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
 import * as Animatable from 'react-native-animatable'
+import { useRef } from "react";
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
+
+    const view = useRef();
+
     const isLeftSwipe = ({ dx }) => dx < -200;
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            view.current
+                .rubberBand(1000)
+                .then((endState) =>
+                    console.log(endState.finished ? 'finished' : 'canceled')
+                );
+        },
         onPanResponderEnd: (e, gestureState) => {
             console.log(gestureState)
             if (isLeftSwipe(gestureState)) {
                 Alert.alert(
                     'Add Favorite',
                     'Are you sure you wish to add ' +
-                        campsite.name +
-                        ' to favorites?',
+                    campsite.name +
+                    ' to favorites?',
                     [
                         {
                             text: 'Cancel',
@@ -34,8 +46,8 @@ const RenderCampsite = (props) => {
                 );
             }
         }
-            
-});
+
+    });
 
     if (campsite) {
         return (
@@ -44,6 +56,7 @@ const RenderCampsite = (props) => {
                 duration={2000}
                 delay={1000}
                 {...panResponder.panHandlers}
+                ref={view}
             >
                 <Card containerStyle={styles.cardContainer}>
                     <Card.Image source={{ uri: baseUrl + campsite.image }}>
