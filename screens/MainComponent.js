81b559a808075021f1c3dfaad5aaabb2c562fa9b
Drew McPherson <drewmac1971@gmail.com>
2023-05-16 13:14:13 -0500

    Secure Store

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -17,6 +17,7 @@ import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import FavoritesScreen from "./FavoritesScreen";
+import LoginScreen from "./LoginScreen";
 
 
 
@@ -170,11 +171,32 @@ const FavoritesNavigator = () => {
                         />
                     )
                 })}
-
             />
         </Stack.Navigator>
     )
 }
+
+
+const LoginNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen name='Login'
+                component={LoginScreen}
+                options={({ navigation }) => ({
+                    headerLeft: () => (
+                        <Icon
+                            name='sign-in'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    );
+};
 
 
 const CustomDrawerContent = (props) => (
@@ -215,6 +237,23 @@ const Main = () => {
                 drawerContent={CustomDrawerContent}
                 drawerStyle={{ backgroundColor: '#CEC8FF' }}
             >
+                <Drawer.Screen
+                    name='Login'
+                    component={LoginNavigator}
+                    options={{
+                        drawerIcon: ({ color }) => (
+                            <Icon
+                                name='sign-in'
+                                type='font-awesome'
+                                size={24}
+                                iconStyle={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+
+                    }}
+                />
+
                 <Drawer.Screen
                     name='Home'
                     component={HomeNavigator}
@@ -346,6 +385,6 @@ const styles = StyleSheet.create({
         color: '#fff',
         fontSize: 24
     }
-})
+});
 
 export default Main;
