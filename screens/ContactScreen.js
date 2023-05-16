b6b6b682b9b2a474890dde7e4df6afd22db527fc
Drew McPherson <drewmac1971@gmail.com>
2023-05-16 14:55:25 -0500

    Sending Email

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,11 +1,21 @@
 import { ScrollView, Text } from "react-native";
-import { Card } from "react-native-elements";
-import * as Animatable from 'react-native-animatable'
+import { Card, Button, Icon } from "react-native-elements";
+import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 
 
 
 const ContactScreen = () => {
+
+    const sendMail = () => {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        });
+    };
+
     return (
         <ScrollView>
             <Animatable.View
@@ -33,6 +43,19 @@ const ContactScreen = () => {
                     <Text>
                         Email: campsites@nucamp.co
                     </Text>
+                    <Button
+                        title='Send Email'
+                        buttonStyle={{ backgroundColor: '#5637DD', margin: 40 }}
+                        icon={
+                            <Icon
+                                name='envelope-o'
+                                type='font-awesome'
+                                color='#fff'
+                                iconStyle={{ marginRight: 10 }}
+                            />
+                        }
+                        onPress={() => sendMail()}
+                    />
                 </Card>
             </Animatable.View>
         </ScrollView>
